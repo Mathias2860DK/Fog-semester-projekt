e1f79df7307cdc986deae7788a0b2e087c63e019
MustafaTokmak1 <mustafatokmak@Mustafas-MacBook-Pro.local>
2021-05-17 23:50:28 +0200

    calculations to more parts

--- src/main/java/business/calculations/CalcPart.java
+++ src/main/java/business/calculations/CalcPart.java
@@ -16,17 +16,102 @@ return amountOfPosts;
 
     public static int calcRafters(int carportLength, int spaceBetweenRafters){//rafters = spær //spaceBetweenRafters is always 55(for now)
         //(Carport længde-10) / 55 +1 =  15
-return (carportLength-10)/spaceBetweenRafters+1;
+        int amount = 0;
+        amount = (carportLength-10)/spaceBetweenRafters+1;
+return amount;
     }
     public static int calcRem(int carportLength,int remLength){//rem engelsk?
+        //TODO tjek
         int remAmount = ((carportLength*2)/remLength)+1;
 
 
         return remAmount;
     }
+
     public static int calcRoof(int carportLength, int carportWidth, int roofLength, int roofWidth){
         int amount = ((carportLength/roofLength)+1)*((carportWidth/roofWidth)+1);
         return amount;
     }
+
+
+    public static int calcSubStarboardsBackAndFront(int carportWidth, int subStarboardBackAndFrontLength){
+        //((carportWidth+5)/subStarboardLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if(carportWidth<subStarboardBackAndFrontLength){
+            amount = 2;
+        } else if(carportWidth>subStarboardBackAndFrontLength && carportWidth < (subStarboardBackAndFrontLength*2)){
+            amount = 4;
+        } else if(carportWidth>(subStarboardBackAndFrontLength*2)){
+            amount = 6;
+        }
+
+        return amount;
+    }
+
+    public static int calcSubStarboardsSides(int carportLength, int subStarboardSidesLength){
+        //((carportLength+5)/subStarboardLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if(carportLength<subStarboardSidesLength){
+            amount = 2;
+        } else if(carportLength>subStarboardSidesLength){
+            amount = 4;
+        }
+
+        return amount;
+    }
+    public static int calcOverSternFor(int carportWidth, int overSternForLength){
+        //((carportWidth+5)/overSternForLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if(carportWidth<overSternForLength){
+            amount = 2;
+        } else if(carportWidth>overSternForLength && carportWidth < (overSternForLength*2)){
+            amount = 4;
+        } else if(carportWidth>(overSternForLength*2)){
+            amount = 6;
+        }
+
+        return amount;
+    }
+    public static int calcOverSternSider(int carportLength, int overSternSiderLength) {
+        //((carportLength+5)/OverSternSiderLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if (carportLength < overSternSiderLength) {
+            amount = 2;
+        } else if (carportLength > overSternSiderLength) {
+            amount = 4;
+        }
+        return amount;
+
+    }
+    public static int calcVandBrædtSider(int carportLength, int vandBrædtSiderLength) {
+        //((carportLength+5)/vandBrædtSiderLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if (carportLength < vandBrædtSiderLength) {
+            amount = 2;
+        } else if (carportLength > vandBrædtSiderLength) {
+            amount = 4;
+        }
+        return amount;
+    }
+    public static int calcVandBrædtFront(int carportWidth, int vandBrædtFrontLength){
+        //((carportWidth+5)/overSternForLength)*2+1; is the best for economical reasons,
+        // but the following is beautiful and manageable;
+        int amount = 0;
+        if(carportWidth<vandBrædtFrontLength){
+            amount = 1;
+        } else if(carportWidth>vandBrædtFrontLength ){
+            amount = 2;
+        } else if(carportWidth>(vandBrædtFrontLength*2)){
+            amount = 3;
+        }
+
+        return amount;
+    }
+
 
 }
