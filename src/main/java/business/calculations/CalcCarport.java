cbccb3ac1b660733b8c68ee0a6226d0c8984cc84
MustafaTokmak1 <mustafatokmak@Mustafas-MacBook-Pro.local>
2021-05-19 12:24:47 +0200

    last calculs

--- src/main/java/business/calculations/CalcCarport.java
+++ src/main/java/business/calculations/CalcCarport.java
@@ -15,52 +15,127 @@ public class CalcCarport {
         int carportLength = order.getCarport().getCarportLength();
         int carportWidth = order.getCarport().getCarportWidth();
         double totalPrice = 0;
+        int roofLength = 0;
+        int roofWidth = 0;
+        int amountOfUniversalFittingsRight = 0;
+        int amountOfUniversalFittingsLeft = 0;
 
         for (Material thisMaterial : materialsFacade.getAllMaterials()) {
+
             if (thisMaterial.getMaterialId() == 4) {
                 thisMaterial.setAmount(CalcPart.calcSubStarboardsBackAndFront(carportWidth, thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 5) {
                 thisMaterial.setAmount(CalcPart.calcSubStarboardsSides(carportLength, thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 6) {
                 thisMaterial.setAmount(CalcPart.calcOverSternFor(carportWidth, thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 7) {
                 thisMaterial.setAmount(CalcPart.calcOverSternSider(carportLength, thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 8) {
                 thisMaterial.setAmount(0);//ikke oprettet
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 9) {
                 thisMaterial.setAmount(0);//ikke oprettet
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 10) {
                 thisMaterial.setAmount(0);//ikke oprettet
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 11) {
-                thisMaterial.setAmount(CalcPart.calcRem(carportLength,thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                thisMaterial.setAmount(CalcPart.calcRem(carportLength, thisMaterial.getLength()));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 12) {
                 thisMaterial.setAmount(0);//ikke oprettet
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 13) {
-                thisMaterial.setAmount(CalcPart.calcRafters(carportLength,55));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                thisMaterial.setAmount(CalcPart.calcRafters(carportLength, 55));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 14) {
                 thisMaterial.setAmount(CalcPart.calcPostAmount(carportLength));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
             } else if (thisMaterial.getMaterialId() == 15) {
                 thisMaterial.setAmount(0);//ikke oprettet
+
             } else if (thisMaterial.getMaterialId() == 16) {
-                thisMaterial.setAmount(CalcPart.calcVandBrædtSider(carportLength,thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
-            }else if(thisMaterial.getMaterialId()==17){
-                thisMaterial.setAmount(CalcPart.calcVandBrædtFront(carportWidth,thisMaterial.getLength()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
-            } else if(thisMaterial.getMaterialId()==18){
-                thisMaterial.setAmount(CalcPart.calcRoof(carportLength,carportWidth,thisMaterial.getLength(),thisMaterial.getWidth()));
-                totalPrice = totalPrice + (thisMaterial.getPrice()* thisMaterial.getAmount());
+                thisMaterial.setAmount(CalcPart.calcVandBrædtSider(carportLength, thisMaterial.getLength()));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 17) {
+                thisMaterial.setAmount(CalcPart.calcVandBrædtFront(carportWidth, thisMaterial.getLength()));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 18) {
+                roofLength = thisMaterial.getLength();
+                roofWidth = thisMaterial.getWidth();
+                thisMaterial.setAmount(CalcPart.calcRoof(carportLength, carportWidth, roofLength, roofWidth));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 19) {
+                thisMaterial.setAmount(CalcPart.calcPlastmoScrew(CalcPart.calcRoof(carportLength, carportWidth, roofLength, roofWidth)));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 20) {
+                thisMaterial.setAmount(CalcPart.calcPerforatedTape());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 21) {
+                amountOfUniversalFittingsRight = CalcPart.calcUniversalFittingsRight(CalcPart.calcRafters(carportLength, 55));
+                thisMaterial.setAmount(amountOfUniversalFittingsRight);
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 22) {
+                amountOfUniversalFittingsLeft = CalcPart.calcUniversalFittingsLeft(CalcPart.calcRafters(carportLength, 55));
+                thisMaterial.setAmount(amountOfUniversalFittingsLeft);
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 23) {
+                thisMaterial.setAmount(CalcPart.calcScrews60mm());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 24) {
+                thisMaterial.setAmount(CalcPart.calcFittingScrews(amountOfUniversalFittingsRight, amountOfUniversalFittingsLeft));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 25) {
+                thisMaterial.setAmount(CalcPart.calcPostAmount(carportLength));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 26) {
+                thisMaterial.setAmount(CalcPart.calcSquareWasher(CalcPart.calcPostAmount(carportLength)));
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 27) {
+                thisMaterial.setAmount(CalcPart.calcScrews70mm());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 28) {
+                thisMaterial.setAmount(CalcPart.calcScrews50mm());
+                totalPrice = totalPrice + (thisMaterial.getPrice() * thisMaterial.getAmount());
+
+            } else if (thisMaterial.getMaterialId() == 29) {
+                thisMaterial.setAmount(0);//ikke oprettet
+
+
+            } else if (thisMaterial.getMaterialId() == 30) {
+                thisMaterial.setAmount(0);//ikke oprettet
+
+
+            } else if (thisMaterial.getMaterialId() == 31) {
+                thisMaterial.setAmount(0);//ikke oprettet
+
             }
 
         }
@@ -72,6 +147,10 @@ public class CalcCarport {
     public Carport getCarportWithMaterials(Carport carport, MaterialsFacade materialsFacade) throws UserException {
         int carportLength = carport.getCarportLength();
         int carportWidth = carport.getCarportWidth();
+        int roofLength = 0;
+        int roofWidth = 0;
+        int amountOfUniversalFittingsRight = 0;
+        int amountOfUniversalFittingsLeft = 0;
 
 
         List<Material> materialList = carport.getMaterialList();
@@ -84,12 +163,15 @@ public class CalcCarport {
             } else if (thisMaterial.getMaterialId() == 5) {
                 thisMaterial.setAmount(CalcPart.calcSubStarboardsSides(carportLength, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
+
             } else if (thisMaterial.getMaterialId() == 6) {
                 thisMaterial.setAmount(CalcPart.calcOverSternFor(carportWidth, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
+
             } else if (thisMaterial.getMaterialId() == 7) {
                 thisMaterial.setAmount(CalcPart.calcOverSternSider(carportLength, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
+
             } else if (thisMaterial.getMaterialId() == 8) {
                 thisMaterial.setAmount(0);//ikke oprettet
                 materialList.add(thisMaterial);
@@ -103,7 +185,7 @@ public class CalcCarport {
                 materialList.add(thisMaterial);
 
             } else if (thisMaterial.getMaterialId() == 11) {
-                thisMaterial.setAmount(CalcPart.calcRem(carportLength,thisMaterial.getLength()));
+                thisMaterial.setAmount(CalcPart.calcRem(carportLength, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
 
             } else if (thisMaterial.getMaterialId() == 12) {
@@ -111,7 +193,7 @@ public class CalcCarport {
                 materialList.add(thisMaterial);
 
             } else if (thisMaterial.getMaterialId() == 13) {
-                thisMaterial.setAmount(CalcPart.calcRafters(carportLength,55));
+                thisMaterial.setAmount(CalcPart.calcRafters(carportLength, 55));
                 materialList.add(thisMaterial);
 
             } else if (thisMaterial.getMaterialId() == 14) {
@@ -121,22 +203,77 @@ public class CalcCarport {
             } else if (thisMaterial.getMaterialId() == 15) {
                 thisMaterial.setAmount(0);//ikke oprettet
                 materialList.add(thisMaterial);
+
             } else if (thisMaterial.getMaterialId() == 16) {
-                thisMaterial.setAmount(CalcPart.calcVandBrædtSider(carportLength,thisMaterial.getLength()));
+                thisMaterial.setAmount(CalcPart.calcVandBrædtSider(carportLength, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
 
-            }else if(thisMaterial.getMaterialId()==17){
-                thisMaterial.setAmount(CalcPart.calcVandBrædtFront(carportWidth,thisMaterial.getLength()));
+            } else if (thisMaterial.getMaterialId() == 17) {
+                thisMaterial.setAmount(CalcPart.calcVandBrædtFront(carportWidth, thisMaterial.getLength()));
                 materialList.add(thisMaterial);
 
-            } else if(thisMaterial.getMaterialId()==18){
-                thisMaterial.setAmount(CalcPart.calcRoof(carportLength,carportWidth,thisMaterial.getLength(),thisMaterial.getWidth()));
+            } else if (thisMaterial.getMaterialId() == 18) {
+                roofLength = thisMaterial.getLength();
+                roofWidth = thisMaterial.getWidth();
+                thisMaterial.setAmount(CalcPart.calcRoof(carportLength, carportWidth, roofLength, roofWidth));
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 19) {
+                thisMaterial.setAmount(CalcPart.calcPlastmoScrew(CalcPart.calcRoof(carportLength, carportWidth, roofLength, roofWidth)));
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 20) {
+                thisMaterial.setAmount(CalcPart.calcPerforatedTape());
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 21) {
+                amountOfUniversalFittingsRight = CalcPart.calcUniversalFittingsRight(CalcPart.calcRafters(carportLength, 55));
+                thisMaterial.setAmount(amountOfUniversalFittingsRight);
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 22) {
+                amountOfUniversalFittingsLeft = CalcPart.calcUniversalFittingsLeft(CalcPart.calcRafters(carportLength, 55));
+                thisMaterial.setAmount(amountOfUniversalFittingsLeft);
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 23) {
+                thisMaterial.setAmount(CalcPart.calcScrews60mm());
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 24) {
+                thisMaterial.setAmount(CalcPart.calcFittingScrews(amountOfUniversalFittingsRight, amountOfUniversalFittingsLeft));
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 25) {
+                thisMaterial.setAmount(CalcPart.calcPostAmount(carportLength));
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 26) {
+                thisMaterial.setAmount(CalcPart.calcSquareWasher(CalcPart.calcPostAmount(carportLength)));
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 27) {
+                thisMaterial.setAmount(CalcPart.calcScrews70mm());
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 28) {
+                thisMaterial.setAmount(CalcPart.calcScrews50mm());
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 29) {
+                thisMaterial.setAmount(0);//ikke oprettet
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 30) {
+                thisMaterial.setAmount(0);//ikke oprettet
+                materialList.add(thisMaterial);
+
+            } else if (thisMaterial.getMaterialId() == 31) {
+                thisMaterial.setAmount(0);//ikke oprettet
                 materialList.add(thisMaterial);
 
             }
-
         }
-
 
         return carport;
     }
--- src/main/java/business/calculations/CalcPart.java
+++ src/main/java/business/calculations/CalcPart.java
@@ -1,80 +1,86 @@
 package business.calculations;
 
+import com.mysql.cj.xdevapi.UpdateStatementImpl;
+
 public class CalcPart {
     final static int MAX_LENGTH_BETWEEN_POSTS = 310;
-    public static int calcPostAmount(int carportLength){//evt boolean med shed eller ej
+
+    public static int calcPostAmount(int carportLength) {//evt boolean med shed eller ej
         //100cm from front to first post
         //30 cm from the back of carport is where the post is located.
-        int distance = carportLength-100-30; //distance between front and rear posts.
-        int amountOfPosts = distance/MAX_LENGTH_BETWEEN_POSTS;//int converts it downwards.
+        int distance = carportLength - 100 - 30; //distance between front and rear posts.
+        int amountOfPosts = distance / MAX_LENGTH_BETWEEN_POSTS;//int converts it downwards.
         amountOfPosts = amountOfPosts * 2; //calculating the posts needed for both sides
         amountOfPosts = amountOfPosts + 4; //always 2 posts at the front and back of the carport.
         //TODO: what if there is a shed?
         //if (isShedWanted == true) ....
-return amountOfPosts;
+        return amountOfPosts;
     }
 
-    public static int calcRafters(int carportLength, int spaceBetweenRafters){//rafters = spær //spaceBetweenRafters is always 55(for now)
+    public static int calcRafters(int carportLength, int spaceBetweenRafters) {//rafters = spær //spaceBetweenRafters is always 55(for now)
         //(Carport længde-10) / 55 +1 =  15
         int amount = 0;
-        amount = (carportLength-10)/spaceBetweenRafters+1;
-return amount;
+        amount = (carportLength - 10) / spaceBetweenRafters + 1;
+        return amount;
     }
-    public static int calcRem(int carportLength,int remLength){//rem engelsk?
+
+    public static int calcRem(int carportLength, int remLength) {//rem engelsk?
         //TODO tjek
-        int remAmount = ((carportLength*2)/remLength)+1;
+        int remAmount = ((carportLength * 2) / remLength) + 1;
 
 
         return remAmount;
     }
 
-    public static int calcRoof(int carportLength, int carportWidth, int roofLength, int roofWidth){
-        int amount = ((carportLength/roofLength)+1)*((carportWidth/roofWidth)+1);
+    public static int calcRoof(int carportLength, int carportWidth, int roofLength, int roofWidth) {
+        int amount = ((carportLength / roofLength) + 1) * ((carportWidth / roofWidth) + 1);
         return amount;
     }
 
 
-    public static int calcSubStarboardsBackAndFront(int carportWidth, int subStarboardBackAndFrontLength){
+    public static int calcSubStarboardsBackAndFront(int carportWidth, int subStarboardBackAndFrontLength) {
         //((carportWidth+5)/subStarboardLength)*2+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
         int amount = 0;
-        if(carportWidth<subStarboardBackAndFrontLength || carportWidth == subStarboardBackAndFrontLength){
+        if (carportWidth < subStarboardBackAndFrontLength || carportWidth == subStarboardBackAndFrontLength) {
             amount = 2;
-        } else if(carportWidth>subStarboardBackAndFrontLength || carportWidth < (subStarboardBackAndFrontLength*2) || carportWidth == (subStarboardBackAndFrontLength*2)){
+        } else if (carportWidth > subStarboardBackAndFrontLength && carportWidth < (subStarboardBackAndFrontLength * 2) || carportWidth == (subStarboardBackAndFrontLength * 2)) {
             amount = 4;
-        } else if(carportWidth>(subStarboardBackAndFrontLength*2)){
+        } else if (carportWidth > (subStarboardBackAndFrontLength * 2)) {
             amount = 6;
         }
 
         return amount;
     }
 
-    public static int calcSubStarboardsSides(int carportLength, int subStarboardSidesLength){
+    public static int calcSubStarboardsSides(int carportLength, int subStarboardSidesLength) {
         //(carportLength/subStarboardLength)*2+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
         int amount = 0;
-        if(carportLength<subStarboardSidesLength || carportLength == subStarboardSidesLength){
+        if (carportLength < subStarboardSidesLength || carportLength == subStarboardSidesLength) {
             amount = 2;
-        } else if(carportLength>subStarboardSidesLength){
+        } else if (carportLength > subStarboardSidesLength) {
             amount = 4;
         }
 
         return amount;
     }
-    public static int calcOverSternFor(int carportWidth, int overSternForLength){
+
+    public static int calcOverSternFor(int carportWidth, int overSternForLength) {
         //((carportWidth+5)/overSternForLength)*2+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
         int amount = 0;
-        if(carportWidth<overSternForLength || carportWidth == overSternForLength){
+        if (carportWidth < overSternForLength || carportWidth == overSternForLength) {
             amount = 1;
-        } else if(carportWidth>overSternForLength || carportWidth < (overSternForLength*2) || carportWidth == (overSternForLength*2)){
+        } else if (carportWidth > overSternForLength && carportWidth < (overSternForLength * 2) || carportWidth == (overSternForLength * 2)) {
             amount = 2;
-        } else if(carportWidth>(overSternForLength*2)){
+        } else if (carportWidth > (overSternForLength * 2)) {
             amount = 3;
         }
 
         return amount;
     }
+
     public static int calcOverSternSider(int carportLength, int overSternSiderLength) {
         //(carportLength/OverSternSiderLength)*2+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
@@ -87,6 +93,7 @@ return amount;
         return amount;
 
     }
+
     public static int calcVandBrædtSider(int carportLength, int vandBrædtSiderLength) {
         //(carportLength/vandBrædtSiderLength)*2+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
@@ -98,19 +105,70 @@ return amount;
         }
         return amount;
     }
-    public static int calcVandBrædtFront(int carportWidth, int vandBrædtFrontLength){
+
+    public static int calcVandBrædtFront(int carportWidth, int vandBrædtFrontLength) {
         //((carportWidth+5)/overSternForLength)+1; is the best for economical reasons,
         // but the following is beautiful and manageable;
         int amount = 0;
-        if(carportWidth<vandBrædtFrontLength || carportWidth == vandBrædtFrontLength){
+        if (carportWidth < vandBrædtFrontLength || carportWidth == vandBrædtFrontLength) {
             amount = 1;
-        } else if(carportWidth>vandBrædtFrontLength || carportWidth < (vandBrædtFrontLength*2) || carportWidth == (vandBrædtFrontLength*2) ){
+        } else if (carportWidth > vandBrædtFrontLength && carportWidth < (vandBrædtFrontLength * 2) || carportWidth == (vandBrædtFrontLength * 2)) {
             amount = 2;
-        } else if(carportWidth>(vandBrædtFrontLength*2)){
+        } else if (carportWidth > (vandBrædtFrontLength * 2)) {
             amount = 3;
         }
 
         return amount;
+    }
+
+    public static int calcPlastmoScrew(int roof) {
+        int amount = 0;
+        if (roof < 3 || roof == 3) {
+            amount = 1;
+        } else if (roof > 3 && roof < 6 || roof == 6) {
+            amount = 2;
+        } else if (roof > 6 && roof < 9 || roof == 9) {
+            amount = 3;
+        } else if (roof > 9 && roof < 12 || roof == 12){
+            amount = 4;
+        }
+        return amount;
+    }
+
+    public static int calcPerforatedTape(){
+        return 2;
+    }
+    public static int calcUniversalFittingsRight(int amountOfRafters){
+        return amountOfRafters;
+    }
+    public static int calcUniversalFittingsLeft(int amountOfRafters){
+        return amountOfRafters;
+    }
+    public static int calcScrews60mm(){
+        return 1;
+    }
+    public static int calcFittingScrews(int universalFittingsRight, int universalFittingsLeft){
+        int amount = 0;
+        amount =((((universalFittingsRight + universalFittingsLeft) * 3 * 3) / 250)+1)+1;
+
+        return amount;
+    }
+    public static int calcCarriageBolt(int posts){
+        int amount = 0;
+        amount = posts*2;
+
+        return amount;
+    }
+    public static int calcSquareWasher(int posts){
+        int amount = 0;
+        amount = posts+1;
+        return amount;
+    }
+    public static int calcScrews70mm(){
+        return 2;
+    }
+    public static int calcScrews50mm(){
+        return 2;
     }
 
 
